11f6f1783a2e6d26f09a289c6d4285159697df2c
Segni Dessalegn <segnides@gmail.com>
2023-03-23 12:19:36 +0300

    responsive navigation bar and sidebar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,29 +1,33 @@
 <template>
-    <div class="fixed top-0 left-0 right-0 flex flex-row w-full p-2 bg-indigo-900 h-16 text-white text-xs z-10">
-        <div class="flex flex-row font-bold text-gray-300 items-center w-4/5">
-            <img src="../assets/gitlab.png" class="w-10 h-10 ml-3 p-2 cursor-pointer rounded" />
-            <div class="ml-3 p-2 hover:bg-gray-100/25 cursor-pointer rounded">
+    <div class="fixed top-0 left-0 right-0 flex flex-row justify-end w-full p-2 bg-indigo-900 sm:h-16 h-12 text-white text-xs z-10 whitespace-nowrap">
+        <div class="flex flex-row font-bold text-gray-300 justify-start w-full items-center w-4/5">
+            <img src="../assets/gitlab.png" class="w-10 h-10 ml-3 p-2 cursor-pointer rounded"/>
+            <img src="../assets/search.png" class="w-8 h-8 p-2 md:hidden cursor-pointer m-1 hover:bg-gray-100/25 active:bg-gray-100/50 rounded" />
+            <div class="flex flex-row font-bold text-gray-300 items-center w-4/5 hidden md:flex">
+                <div class="ml-3 p-2 hover:bg-gray-100/25 active:bg-gray-100/50 cursor-pointer rounded">
                 About Gitlab
             </div>
-            <div class="ml-3 p-2 hover:bg-gray-100/25 cursor-pointer rounded">
+            
+            <div class="ml-3 p-2 hover:bg-gray-100/25 active:bg-gray-100/50 cursor-pointer rounded">
                 Pricing
             </div>
-            <div class="ml-3 p-2 hover:bg-gray-100/25 cursor-pointer rounded">
+            <div class="ml-3 p-2 hover:bg-gray-100/25 active:bg-gray-100/50 cursor-pointer rounded">
                 Talk to an expert
             </div>
             <input
-                class="transition-all duration-150 ease-in-out w-1/3 focus:w-3/5 focus:bg-white bg-gray-400/25 hover:bg-gray-100/25 border-1 border-gray-200/50 border p-2 ml-10 rounded outline-0"
+                class="transition-all duration-150 ease-in-out md:w-2/5 w-1/3 focus:w-3/5 focus:bg-white bg-gray-400/25 hover:bg-gray-100/25 border-1 border-gray-200/50 border p-2 ml-10 rounded outline-0"
                 placeholder="Search Gitlab" />
+            </div>
         </div>
         <div class="flex flex-row justify-end items-center w-1/5">
             <img src="../assets/question.png"
-                class="w-9 h-9 text-white ml-3 p-2 hover:bg-gray-100/25 cursor-pointer rounded" />
-            <img src="../assets/more.png" class="w-9 h-9 text-white ml-3 p-2 hover:bg-gray-100/25 cursor-pointer rounded" />
+                class="w-9 h-9 text-white ml-3 p-2 hover:bg-gray-100/25 active:bg-gray-100/50 cursor-pointer rounded hidden sm:block" />
+            <img src="../assets/more.png" class="w-9 h-9 text-white ml-3 p-2 hover:bg-gray-100/25 active:bg-gray-100/50 cursor-pointer rounded block" />
             <div
-                class="bg-white text-black p-2 ml-3 rounded hover:bg-gray-100/25 hover:text-white hover:border-white border cursor-pointer">
+                class="bg-white text-black p-2 ml-3 rounded hover:bg-gray-100/25 active:bg-gray-100/50 hover:text-white hover:border-white border cursor-pointer hidden sm:block">
                 Register
             </div>
-            <div class="cursor-pointer hover:bg-gray-100/25 ml-3 p-2 rounded">
+            <div class="cursor-pointer hover:bg-gray-100/25 active:bg-gray-100/50 ml-3 p-2 rounded hidden sm:block">
                 Sign in
             </div>
         </div>
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,32 +1,32 @@
 <template>
     <div>
         <div class="flex flex-col justify-between fixed top-0 left-0 transition-all duration-200 ease-in-out h-screen z-0 bg-gray-100 border-r-2 border-slate-300 text-xs"
-            :class="[$store.state.SideBarCollapsed ? 'w-12' : 'w-64']">
+            :class="[$store.state.SideBarCollapsed ? 'w-12' : 'lg:w-64 sm:flex hidden']">
             <div class="mt-20 flex-col">
                 <div
                     class="rounded-lg hover:bg-gray-200 p-1.5 mr-1 ml-3 mb-2 font-bold h-8 cursor-pointer flex flex-row justify-start">
                     <img src="../assets/direction.png" class="w-5 h-5 mr-3" />
-                    <div v-if="!$store.state.SideBarCollapsed" class="w-50">Explore</div>
+                    <div v-if="!$store.state.SideBarCollapsed" class="w-50 lg:block hidden">Explore</div>
                 </div>
                 <div class="rounded-lg bg-gray-200 p-1.5 m-1 ml-3 h-8 cursor-pointer font-bold flex flex-row justify-start">
                     <img src="../assets/projects.png" class="w-5 h-5 mr-3" />
-                    <div v-if="!$store.state.SideBarCollapsed" class="w-50">Projects</div>
+                    <div v-if="!$store.state.SideBarCollapsed" class="w-50 lg:block hidden">Projects</div>
                 </div>
                 <div class="rounded-lg hover:bg-gray-200 p-1.5 m-1 ml-3 h-8 cursor-pointer flex flex-row justify-start">
                     <img src="../assets/groups.png" class="w-5 h-5 mr-3" />
-                    <div v-if="!$store.state.SideBarCollapsed" class="w-50">Groups</div>
+                    <div v-if="!$store.state.SideBarCollapsed" class="w-50 lg:block hidden">Groups</div>
                 </div>
                 <div class="rounded-lg hover:bg-gray-200 p-1.5 m-1 ml-3 h-8 cursor-pointer flex flex-row justify-start">
                     <img src="../assets/topics.png" class="w-5 h-5 mr-3" />
-                    <div v-if="!$store.state.SideBarCollapsed" class="w-50">Topics</div>
+                    <div v-if="!$store.state.SideBarCollapsed" class="w-50 lg:block hidden">Topics</div>
                 </div>
                 <div class="rounded-lg hover:bg-gray-200 p-1.5 m-1 ml-3 h-8 cursor-pointer flex flex-row justify-start">
                     <img src="../assets/scissors.png" class="w-5 h-5 mr-3" />
-                    <div v-if="!$store.state.SideBarCollapsed" class="w-50">Snippets</div>
+                    <div v-if="!$store.state.SideBarCollapsed" class="w-50 lg:block hidden">Snippets</div>
                 </div>
             </div>
             <div @click="ToggleSideBar" class="p-2 cursor-pointer text-sm bg-gray-100 hover:bg-gray-200">
-                <div v-if="!$store.state.SideBarCollapsed">Collapse sideBar</div>
+                <div v-if="!$store.state.SideBarCollapsed" class="lg:block hidden">Collapse sideBar</div>
                 <div v-else>>></div>
             </div>
         </div>
